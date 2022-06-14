b8e7c860c8c4b81a4f54c3e0faf7ec8b2dd8d99f
aanmeba <aanmeba@gmail.com>
2022-06-14 15:47:06 +1000

    separates project list in a json file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,158 +1,98 @@
 import HelmetConfig from "../components/HelmetConfig";
-// import { Link } from "react-router-dom";
+import {
+  CardContainer,
+  CardBox,
+  MainContainer,
+  UlWrapper,
+  Ul,
+  Li,
+  HeadingFour,
+  CardText,
+} from "../components/StyledComponents";
 
 const projectList = require("../data/projectdetails.json");
-console.log(projectList[0].title);
-
-// const { id, title, description, github, deployed_link, other_link, language } =
-//   projectList;
 
 const Projects = () => {
   return (
-    <main className="main-section">
+    <MainContainer className="main-section">
       <HelmetConfig pageLocation="Projects" />
-      <h1 className="page-title">Projects</h1>
-
-      <section className="project-container">
-        {projectList.map((proj) => (
-          <div className="project-box">
-            <div className="project-box__link">
-              <a href={proj.github} target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-github"></i>
-              </a>
-            </div>
-            <div className="project-box__content">
-              <h3 className="project-heading">{proj.title}</h3>
-              <h5 className="project-tech">{proj.language[0]}</h5>
-            </div>
-          </div>
-        ))}
-        {/* <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udacity-travel-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">
-              Travel
-              <br />
-              Weather App
-            </h3>
-            <h5 className="project-tech">API JS HTML/CSS</h5>
-          </div>
-        </div>
-        <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udacity-weather-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://dry-journey-77285.herokuapp.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">Weather App</h3>
-            <h5 className="project-tech">API JS HTML/CSS</h5>
-          </div>
-        </div>
-        <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udacity-webpack-api-nlp"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">
-              Sentiment Analysis for News Article
-            </h3>
-            <h5 className="project-tech">API JS HTML/CSS</h5>
-          </div>
-        </div>
-        <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udemy-simon-game"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://aanmeba.github.io/udemy-simon-game/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">Simon Game</h3>
-            <h5 className="project-tech">JS HTML/CSS</h5>
-          </div>
-        </div>
-        <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udacity-landing-page"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://aanmeba.github.io/udacity-landing-page/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">Landing Page</h3>
-            <h5 className="project-tech">JS HTML/CSS</h5>
-          </div>
-        </div>
-        <div className="project-box">
-          <div className="project-box__link">
-            <a
-              href="https://github.com/aanmeba/udacity-personal-blog"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-            <a
-              href="https://aanmeba.github.io/udacity-personal-blog/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-          <div className="project-box__content">
-            <h3 className="project-heading">Personal Blog</h3>
-            <h5 className="project-tech">HTML/CSS</h5>
-          </div>
-        </div> */}
-      </section>
-    </main>
+      <CardContainer className="project-container">
+        {projectList.map((proj) =>
+          proj.title === "" ? null : (
+            <CardBox key={proj.id}>
+              <img
+                src={process.env.PUBLIC_URL + "/img" + proj.image}
+                alt={proj.title}
+                width="100%"
+              />
+              <CardText>
+                <HeadingFour>{proj.title}</HeadingFour>
+                <UlWrapper key={proj.github}>
+                  <Ul>
+                    {proj.deployed_link === "" ? null : (
+                      <>
+                        <Li>
+                          <a
+                            href={proj.deployed_link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Demo
+                          </a>
+                        </Li>
+                        <Li>|</Li>
+                      </>
+                    )}
+                    <Li>
+                      <a
+                        href={proj.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Github
+                      </a>
+                    </Li>
+                    {proj.other_link === "" ? null : (
+                      <>
+                        <Li>|</Li>
+                        <Li>
+                          <a
+                            href={proj.other_link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Slideshow
+                          </a>
+                        </Li>
+                      </>
+                    )}
+                  </Ul>
+                </UlWrapper>
+                <p>{proj.description}</p>
+                <UlWrapper key={proj.tech_stack.length}>
+                  <Ul>
+                    {proj.tech_stack.length === 1 ? (
+                      <Li>{proj.tech_stack[0]}</Li>
+                    ) : (
+                      proj.tech_stack.map((lang, index) =>
+                        proj.tech_stack.length - 1 !== index ? (
+                          <>
+                            <Li key={index}>{lang}</Li>
+                            <Li key={index + 10}>|</Li>
+                          </>
+                        ) : (
+                          <Li key={index}>{lang}</Li>
+                        )
+                      )
+                    )}
+                  </Ul>
+                </UlWrapper>
+              </CardText>
+            </CardBox>
+          )
+        )}
+      </CardContainer>
+    </MainContainer>
   );
 };
 
