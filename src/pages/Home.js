206bf5e0aef9be5726f8af5efb112f8dbe3ce37a
aanmeba <aanmeba@gmail.com>
2022-06-14 15:51:18 +1000

    merges Home and About pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,27 @@
 import React from "react";
 import HelmetConfig from "../components/HelmetConfig";
-import Navigation from "../components/Navigation";
+// import Navigation from "../components/Navigation";
+import {
+  Heading,
+  MainContainer,
+  Highlighter,
+  ExtraHeading,
+} from "../components/StyledComponents";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
-    <main className="container">
+    <MainContainer>
       <HelmetConfig pageLocation="Home" />
-      <section className="title-main">
-        <h1>
-          The <span className="exp">Experimental</span> Playground of
-        </h1>
-      </section>
-      <section className="title-vertical">
-        <h1>Jungah Ahn</h1>
-      </section>
-      <aside className="side-text">
-        <p>Full-stack developer</p>
-      </aside>
-      <footer className="footer">
-        <p>Latest Update: 08/06/2022</p>
-      </footer>
-
-      <Navigation className="navigation" />
-    </main>
+      <ExtraHeading>Hello World!</ExtraHeading>
+      <Heading>
+        I'm <Highlighter>Jungah Ahn</Highlighter> who is a{" "}
+        <Highlighter>junior full-stack developer</Highlighter> with experience
+        building React.js websites and Ruby on Rails web applications, based on
+        Sydney, Australia.
+      </Heading>
+      <Contact />
+    </MainContainer>
   );
 };
 
