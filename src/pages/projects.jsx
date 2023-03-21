7927dba65bb0e2aa8e7fb015d382059cdc5f25da
aanmeba <aanmeba@gmail.com>
2023-03-21 17:36:50 +1100

    remove categories in the project page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,30 +1,19 @@
 import HelmetConfig from "../components/helmet_config";
-import {
-  CardContainer,
-  // CardBox,
-  MainContainer,
-  // UlWrapper,
-  // Ul,
-  // Li,
-  // HeadingFour,
-  // CardText,
-} from "../components/StyledComponents";
-// import LazyLoad from "react-lazyload";
-import { ProjectCategories } from "../components/project_categories";
+import { CardContainer, MainContainer } from "../components/StyledComponents";
+// import { ProjectCategories } from "../components/project_categories";
 import { ProjectSorting } from "../components/project_sorting";
-import { useState } from "react";
 
 const projectList = require("../data/projectdetails.json");
 
 const Projects = () => {
-  const [sorting, setSorting] = useState("project");
+  // const [sorting, setSorting] = useState("project");
 
   return (
     <MainContainer className="main-section">
       <HelmetConfig pageLocation="Projects" />
-      <ProjectCategories sorting={sorting} setSorting={setSorting} />
+      {/* <ProjectCategories sorting={sorting} setSorting={setSorting} /> */}
       <CardContainer className="project-container">
-        <ProjectSorting projectList={projectList} sorting={sorting} />
+        <ProjectSorting projectList={projectList} sorting="project" />
       </CardContainer>
     </MainContainer>
   );
